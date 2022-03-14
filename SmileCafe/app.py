b1cbd987b020d64b10b3319ba39f5e0b5cb34f94
WanderingTraders <jacob.mccabe000@gmail.com>
2022-03-15 11:03:08 +1300

    Uploading the SmileCafe folder

---
+++ SmileCafe/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def render_homepage():
+    return render_template("home.html")
+
+@app.route('/menu')
+def render_menu():
+    return render_template("menu.html")
+
+@app.route('/contact')
+def render_contact():
+    return render_template("contact.html")
+
+app.run(host="0.0.0.0")
